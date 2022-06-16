08cf4387e4f34a82d12fa56f0c00db1b07c24c34
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-06-16 16:36:11 +0100

    Time: 355 ms (34.82%), Space: 28.9 MB (38.33%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (n <= 1) return s;
+        
+        int dp[n + 5][n + 5];
+        string ans = "";
+        memset(dp, 0, sizeof dp);
+        
+        
+        for (int i = n - 1 ; i >= 0 ; i--){
+            for (int j = i ; j < n ; j++){
+                if (i == j) dp[i][j] = 1;
+                else if (j - i == 1 && s[i] == s[j]){
+                    dp[i][j] = 1;
+                }
+                else if (s[i] == s[j] && dp[i + 1][j - 1] == 1){
+                    dp[i][j] = 1;
+                }
+                
+                if (dp[i][j] == 1 && j - i + 1 > ans.size()){
+                    ans =  s.substr(i, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
