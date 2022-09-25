ff7bbe6f99f696c8bab4b30d08f5ed2264b88552
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-09-25 16:49:15 +0100

    Time: 30 ms (78.80%), Space: 17.2 MB (11.06%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -1,62 +1,52 @@
 class MyCircularQueue {
-public:  
-    struct Node{
-        int val;
-        Node* next;
-        Node(int x){
-            val = x;
-            next = NULL;
-        }
-    };
-    Node* front = NULL;
-    Node* rear = NULL;
-    int max_size;
-    int curr_size = 0;
+public:
+    int s=0;
+    int e=0;
+    int siz=0;
+    int maxsize = 0;
+    int arr[3000];
+    
     MyCircularQueue(int k) {
-        max_size = k;
+        maxsize=k;
     }
-    bool enQueue(int value) {   
-        if(curr_size < max_size){
-            Node* new_node = new Node(value);
-            if(front == NULL) {
-                front = new_node;
-                rear = new_node;
-            }
-            else{
-                rear -> next = new_node;
-                rear = new_node;
-            }
-            curr_size++;
-            return true;
-        }
+    
+    bool enQueue(int value) {
+        if(siz == maxsize) return false;
+     
+        arr[e]=value;
+        e++;
+        siz++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if(siz==0) return false;
+		
+        s++;
+        siz--;
+        return true;
+    }
+    
+    int Front() {
+        if(siz==0) return -1;
+		
+        return arr[s];
+    }
+    
+    int Rear() {
+        if(siz==0) return -1;
+		
+        return arr[e-1];    
+    }
+    
+    bool isEmpty() {
+        if(siz==0) return true;
+		
         return false;
     }
-    bool deQueue() {   
-        if(curr_size > 0){
-            Node* temp = front;
-            front = front -> next;
-            temp -> next = NULL;
-            delete temp;
-            curr_size--;
-            return true;
-        }
-        return false;
-    }
-    int Front() {   
-        if(curr_size > 0)
-            return front -> val;
-        return -1;
-    }
-    int Rear() {   
-        if(curr_size > 0)
-            return rear -> val;
-        return -1;
-    }
-    bool isEmpty() {   
-        return curr_size == 0;
-    }
-    bool isFull() {   
-        return curr_size == max_size;
+    
+    bool isFull() {
+        return siz==maxsize;
     }
 };
 
