8750b3748523522a9185b70f293e42a9c3e9dacc
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-10-21 21:06:04 +0100

    Time: 41 ms (61.65%), Space: 13.9 MB (33.86%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,23 +1,37 @@
+/*
+
+    Since we want to find the subarray sum that satisfies the condition, we can
+    sort all the numbers and then find their cummulative sum. This ensures that we
+    get cummulative sum in ascending order.
+    Then using binary search we look for the 1st sum that is greater than the query number.
+    The distance from 0th index to that index-1 is the length of the longest such subarray.
+    
+*/
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        int n = nums.size(), m = queries.size();
-        long long sum = 0, cur;
-        vector<int> ans;
-        for (int i = 0 ; i < n ; i++){
-            sum += nums[i];
+        sort(nums.begin(), nums.end());
+        vector<long long> cumm_sum(nums.size(), 0);
+        
+        // find the cummulative sum
+        for(int i = 0; i < nums.size(); i++)
+            cumm_sum[i] =  nums[i] + (i > 0 ? cumm_sum[i-1] : 0);
+        
+        vector<int> result(queries.size(), 0);
+        for(int i = 0; i < queries.size(); i++) {
+            // Find the position of the subarray sum just greater than current
+            auto it = upper_bound(cumm_sum.begin(), cumm_sum.end(), queries[i]);
+            // position distance from 0
+            int dist = distance(cumm_sum.begin(), it);
+            // No cummulative sum smaller than query found
+            if(it == cumm_sum.begin())
+                result[i] = 0;
+            // When all the nums can be used
+            else if(it == cumm_sum.end())
+                result[i] = cumm_sum.size();
+            else
+                result[i] = dist;
         }
-        sort(nums.rbegin(), nums.rend());
-        for (int i = 0 ; i < m ; i++){
-            cur = sum;
-            int val = queries[i];
-            int j = 0;
-            while (val < cur){
-                cur -= nums[j];
-                j++;
-            }
-            ans.push_back(n - j);
-        }
-        return ans;
+        return result;
     }
 };
