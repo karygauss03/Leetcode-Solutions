615120e18196e207f14099662e43e54e404d41a3
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-06 12:05:45 +0100

    Time: 18 ms (62.25%), Space: 10.2 MB (71.18%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
+    static bool cmp(int a,int b){
+        int resulta = __builtin_popcount(a);
+        int resultb = __builtin_popcount(b);
+        if(resultb == resulta){
+            return a < b;
+        }
+        return resultb > resulta;    
+    }
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int, int>> tmp;
-        for (auto &a : arr){
-            tmp.push_back(make_pair(__builtin_popcount(a), a));
-        }
-        sort(tmp.begin(), tmp.end());
-        vector<int> ans;
-        for (auto &t : tmp){
-            ans.push_back(t.second);
-        }
-        return ans;
+        sort(arr.begin(), arr.end(), cmp);
+        return arr;
     }
 };
