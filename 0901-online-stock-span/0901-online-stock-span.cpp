d3335d056433747381bb3377385da0bb6657bb8d
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-09 09:21:00 +0100

    Time: 730 ms (5.06%), Space: 300.3 MB (5.16%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,17 +1,53 @@
+struct segtree {
+    vector<long long> sums;
+    int size;
+    
+    void init(int n) {
+        size = 1;
+        while (size < n) size *= 2;
+        sums.assign(size * 2, 0LL);
+    }
+    
+    void set(int i, int v, int x, int lx, int rx) {
+        if (rx - lx == 1) {
+            sums[x] = v;
+            return;
+        }
+        int m = (lx + rx) / 2;
+        if (i < m) set(i, v, 2 * x + 1, lx, m);
+        else set(i, v, 2 * x + 2, m, rx);
+        sums[x] = max(sums[2 * x + 1], sums[2 * x + 2]);
+    }
+    
+    void set(int i, int v) {
+        set(i, v, 0, 0, size);
+    }
+    
+    long long sum(int l, int r, int x, int lx, int rx) {
+        if (lx >= r || l >= rx) return 0;
+        if (lx >= l && rx <= r) return sums[x];
+        int m = (lx + rx) / 2;
+        return max(sum(l, r, 2 * x + 1, lx, m), sum(l, r, 2 * x + 2, m, rx));
+    }
+    
+    long long sum(int l, int r) {
+        return sum(l, r, 0, 0, size);
+    }
+};
+
 class StockSpanner {
 public:
-    stack<pair<int, int>> st;
-    StockSpanner() {
+    int mxN = 1e5;
+    int p = 1;
+    segtree st;
+    
+    StockSpanner() { 
+        st.init(mxN);
     }
     
     int next(int price) {
-        int ans = 1;
-        while (st.size() && st.top().first <= price){
-            ans += st.top().second;
-            st.pop();
-        }
-        st.push(make_pair(price, ans));
-        return ans;
+        st.set(price, p);
+        return p++ - st.sum(price + 1, mxN);
     }
 };
 
