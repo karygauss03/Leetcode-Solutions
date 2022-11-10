f1195c66c1d244938a171b43e47fe1c0499aaf44
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-10 12:01:18 +0100

    Time: 9 ms (85.71%), Space: 9 MB (60.84%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -3,12 +3,23 @@ public:
     vector<int> minOperations(string boxes) {
         int n = boxes.size();
         vector<int> ans(n);
+        int count_left = 0, count_right = 0;
+        int sum_left = 0, sum_right = 0;
         for (int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j < n ; j++){
-                if (j == i) continue;
-                if (boxes[j] == '1'){
-                    ans[i] += abs(i - j);
-                }
+            if (boxes[i] == '1'){
+                count_right++;
+                sum_right += i;
+            }
+        }
+        for (int i = 0 ; i < n  ; i ++){
+            if (boxes[i] == '1'){
+                count_right--;
+                sum_right -= i;
+            }
+            ans[i] = abs(sum_right - (count_right*i)) + abs(sum_left - (count_left * i));
+            if (boxes[i] == '1'){
+                count_left++;
+                sum_left += i;
             }
         }
         return ans;
