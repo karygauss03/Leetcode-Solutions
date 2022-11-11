aaef5022662fe3d0974bd116019f64783b9ad5cd
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-11 10:06:35 +0100

    Time: 0 ms (100.00%), Space: 7.6 MB (94.86%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        int dp[n + 1];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2 ; i < n ; i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+};
