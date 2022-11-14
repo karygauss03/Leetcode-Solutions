cda63ef66666ac13713f0ce2ae5169ddd6996c74
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-14 18:42:13 +0100

    Time: 401 ms (54.93%), Space: 14.6 MB (23.02%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    bool vis[17];
-    int n;
-    bool solve(vector<int>& nums, int i, int cur, int target, int k){
-        if (k == 1) return 1;
-        if (cur == target) return solve(nums, 0, 0, target, k - 1);
-        for (int j = i + 1 ; j < n ; j++){
-            if (vis[j] || cur + nums[j] > target) continue;
-            vis[j] = true;
-            if (solve(nums, j, cur + nums[j], target, k)) return true;
-            vis[j] = false;
+    vector<int> masks;
+    int dp[1<<16][17];
+    bool solve(int n, int used, int k){
+        if (k == 0 && used == ((1<<n) - 1)) return 1;
+        if (k == 0 || used == ((1<<n) - 1)) return 0;
+        if (dp[used][k] != -1) return dp[used][k];
+        
+        for (auto &mask : masks){
+            if ((mask & used) == 0 && solve(n, used | mask, k - 1)){
+                return dp[used][k] = 1;
+            }
         }
-        return false;
+        return dp[used][k] = 0;
     }
-    
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        n = nums.size();
+        int n = nums.size();
         int sum = 0;
-        for (auto &num : nums) sum += num;
+        for (auto &x : nums) sum += x;
         if (sum % k) return false;
-        memset(vis, 0, sizeof vis);
-        return solve(nums, 0, 0, sum / k, k);
+        int target = sum / k;
+        for (int i = 0 ; i <(1<<n) ; i++){
+            int cur = 0;
+            for (int j = 0 ; j < n ; j++){
+                if ((1<<j) & i){
+                    cur += nums[j];
+                }
+            }
+            if (cur == target) masks.push_back(i);
+        }
+        memset(dp, -1, sizeof dp);
+        return solve(n, 0, k);
     }
 };
