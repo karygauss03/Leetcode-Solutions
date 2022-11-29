7792c7f916320677e28597d754ff650efe77eb82
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-11-29 09:47:18 +0100

    Time: 8 ms (94.34%), Space: 7.4 MB (78.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int dp[505][505];   
     int minDistance(string s, string t) {
-        int n = s.size();
-        int m = t.size();
+        int n = s.size(), m = t.size();
+        int dp[n + 5][m + 5];
         for (int i = 0 ; i <= n ; i++) dp[i][0] = i;
         for (int j = 0 ; j <= m ; j++) dp[0][j] = j;
         for (int i = 1 ; i <= n ; i++){
@@ -12,8 +11,7 @@ public:
                     dp[i][j] = dp[i - 1][j - 1];
                 }
                 else {
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]);
-                    dp[i][j] = 1 + min(dp[i][j], dp[i - 1][j - 1]);
+                    dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1]));
                 }
             }
         }
