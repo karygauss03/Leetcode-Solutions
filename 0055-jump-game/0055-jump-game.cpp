2fdbe2c4add8ced16923b1cc1bff0c9520f78116
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2022-12-26 12:14:52 +0100

    Time: 77 ms (82.29%), Space: 48.4 MB (79.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,7 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
         int cur; //maximum element can be reached a moment
         cur = nums[0];// at first the maximum element we can reach is nums[0]
-        for (int i = 0 ; i < nums.size() ; i++){
+        for (int i = 0 ; i < nums.size() ; ++i){
             if (cur < i){
                 return false;
             }
