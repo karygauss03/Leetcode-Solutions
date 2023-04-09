6f61ac383446a7dd86895cbaf0172f47b287c661
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-04-09 12:16:52 +0100

    Time: 595 ms (82.90%), Space: 170.1 MB (63.25%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int largestPathValue(string colors, vector<vector<int>>& edges) {
+        int n = colors.length();
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+
+        for (auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
+        }
+        vector<vector<int>> count(n, vector<int>(26));
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        int answer = 0, nodesSeen = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            answer = max(answer, ++count[node][colors[node] - 'a']);
+            nodesSeen++;
+
+            for (auto& neighbor : adj[node]) {
+                for (int i = 0; i < 26; i++) {
+                    count[neighbor][i] = max(count[neighbor][i], count[node][i]);
+                }
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+        return nodesSeen < n ? -1 : answer;
+    }
+};
