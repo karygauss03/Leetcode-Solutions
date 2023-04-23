fdb5653c17d1f371dfbcd3b06a9adfaf8beaba68
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-04-23 11:44:00 +0100

    Time: 37 ms (95.35%), Space: 9.3 MB (98.84%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     const int mod = 1e9 + 7;
-    long dp[100001];
-    int n;
+    int dp[12], n, id;
     int numberOfArrays(string s, int k) {
         n = s.size();
-        dp[n] = 1;
+        dp[n % 12] = 1;
         for (int i = n - 1; i >= 0; --i) {
             long cur = 0, tmp = 0;
+            id = i % 12;
             for (int j = i ; j < s.size() ; ++j) {
                 cur = (cur * 10) + (s[j] - '0');
                 if (cur < 1 || cur > k)
                     break;
-                tmp = (tmp + dp[j + 1]);
+                tmp = (tmp + dp[(j + 1) % 12]);
             }
-            dp[i] = tmp % mod;
+            dp[id] = tmp % mod;
         }
         return dp[0];
     }
