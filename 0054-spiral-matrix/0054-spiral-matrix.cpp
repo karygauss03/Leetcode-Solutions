01d1742e98f086859e5574e4e27b9a4ab1ded6ca
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-05-09 09:24:34 +0100

    Time: 0 ms (100.00%), Space: 6.8 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
+    int i;
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> ans;
-        int n = matrix.size(), m = matrix[0].size();
-        int sr = 0, sc = 0, er = n - 1, ec = m - 1;
+        int sr = 0, sc = 0, er = matrix.size() - 1, ec = matrix[0].size() - 1;
         while (sr <= er && sc <= ec) {
-            for (int i = sc ; i <= ec && sr <= er ; ++i) {
+            for (i = sc ; i <= ec && sr <= er ; ++i) {
                 ans.push_back(matrix[sr][i]);
             }
             ++sr;
             
-            for (int i = sr ; i <= er && sc <= ec ; ++i) {
+            for (i = sr ; i <= er && sc <= ec ; ++i) {
                 ans.push_back(matrix[i][ec]);
             }
             --ec;
             
-            for(int i = ec ; i >= sc && sr <= er; --i){
+            for(i = ec ; i >= sc && sr <= er; --i){
                 ans.push_back(matrix[er][i]);
             }
             --er;
             
-            for (int i = er ; i >= sr && sc <= ec ; --i) {
+            for (i = er ; i >= sr && sc <= ec ; --i) {
                 ans.push_back(matrix[i][sc]);
             }
             ++sc;
