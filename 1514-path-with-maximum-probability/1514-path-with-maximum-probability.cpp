e46da8c6e24b746b282e911f029281302b09158a
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-06-28 19:22:18 +0100

    Time: 211 ms (69.67%), Space: 64.9 MB (75.49%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    bool vis[10005];
     vector<pair<int, double>> adj[10005];
-    double ans = 0.0;
-    
-    void dfs(int node, int end, double cur) {
-        if (cur <= 1e-5) return;
-        if (node == end) {
-            ans = max(ans, cur);
-            return;
-        }
-        vis[node] = 1;
-        for (auto &u : adj[node]){
-            if (!vis[u.first] && cur * u.second >= ans) {
-                dfs(u.first, end, cur * u.second);
-            }
-        }
-        vis[node] = 0;
-    } 
+    priority_queue<pair<double, int>> pq;
     
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        memset(vis, 0, sizeof vis);
         for (int i = 0 ; i < edges.size() ; ++i) {
             adj[edges[i][0]].push_back(make_pair(edges[i][1], succProb[i]));
             adj[edges[i][1]].push_back(make_pair(edges[i][0], succProb[i]));
         }
-        dfs(start, end, 1);
-        return ans;
+        pq.push({1.0, start});
+        vector<double> probabilities(n + 1, 0.0);
+        probabilities[start] = 1.0;
+        while (!pq.empty()) {
+            pair<double, int> topElement = pq.top();
+            pq.pop();
+            if (topElement.second == end) {
+                continue;
+            } 
+            for (auto &u : adj[topElement.second]) {
+                if (probabilities[u.first] < topElement.first * u.second) {
+                    probabilities[u.first] = topElement.first * u.second;
+                    pq.push({probabilities[u.first], u.first});
+                }
+            }
+        }
+        return probabilities[end];
     }
 };
