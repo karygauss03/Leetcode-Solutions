d16dffeef6210aecfe4ffa0fb8a5d66dbf9e6140
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-07-15 17:28:05 +0100

    Time: 286 ms (95.73%), Space: 75.9 MB (58.61%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -6,8 +6,13 @@ public:
         if (dp[cur][k] != -1) return dp[cur][k];
         int ans = solve(events, k, cur + 1, dp); // Ignoring the cur-th event and move to the next event
         if (k) {
-            vector<int> temp = {events[cur][1],INT_MAX,INT_MAX};
-            int nx = upper_bound(events.begin(), events.end(), temp) - events.begin();
+            int nx = events.size();
+            for (int i = cur + 1 ; i < events.size() ; ++i) {
+                if (events[i][0] > events[cur][1]) {
+                    nx = i;
+                    break;
+                }
+            }
             ans = max(ans, events[cur][2] + solve(events, k - 1, nx, dp)); // taking the cur-th event and move to the next possible event
         }
         return dp[cur][k] = ans;
