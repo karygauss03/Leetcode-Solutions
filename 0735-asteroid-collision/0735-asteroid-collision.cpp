ee5a68ddc209409352af52c20957f638a931ae03
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-07-20 13:52:29 +0100

    Time: 11 ms (92.72%), Space: 17.9 MB (24.17%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
+            }
+            else {
+                while(!s.empty() && s.top() > 0 && s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() && s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0) {
+                        s.push(ast[i]);
+                    }
+                }
+            }
+        }
+        vector<int> res(s.size());
+        for(int i = (int)s.size() - 1; i >= 0; i--) {
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
+    }
+};
