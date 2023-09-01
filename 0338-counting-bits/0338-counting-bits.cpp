80578ee70e85e8da712e120ec0985fbc5fef0bd6
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-09-01 08:52:55 +0100

    Time: 0 ms (100.00%), Space: 8.6 MB (14.67%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for (int i = 0 ; i <= n ; ++i) {
+            ans.push_back(__builtin_popcount(i));
+        }
+        return ans;
+    }
+};
