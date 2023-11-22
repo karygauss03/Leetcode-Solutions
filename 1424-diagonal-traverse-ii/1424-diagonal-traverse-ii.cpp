110eb900f44a820d7c8b215db28daa5b2a583d98
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-11-22 08:56:47 +0100

    Time: 123 ms (86.97%), Space: 66.1 MB (90.60%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        map<int, vector<int>> valuesBySum;
-        for (int i = 0 ; i < nums.size() ; ++i) {
-            for (int j = 0 ; j < nums[i].size() ; ++j) {
-                valuesBySum[i + j].push_back(nums[i][j]);
-                
-            }
+        if (nums.empty() || nums[0].empty()) {
+            return {};
         }
+        
         vector<int> ans;
-        for (auto &m : valuesBySum) {
-            int sum = 0;
-            for (int i = m.second.size() - 1 ; i >= 0 ; --i) {
-                ans.push_back(m.second[i]);
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        while (!q.empty()) {
+            pair<int, int> front = q.front();
+            ans.push_back(nums[front.first][front.second]);
+            if (front.first + 1 < nums.size() && front.second == 0) {
+                q.push({front.first + 1, front.second});
             }
+            if (front.second + 1 < nums[front.first].size()) {
+                q.push({front.first, front.second + 1});
+            }
+            q.pop();
         }
+        
         return ans;
     }
 };
