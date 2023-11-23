ffe5974a85e0499bbbde45352b7ec117cf7af03a
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-11-23 11:43:20 +0100

    Time: 59 ms (55.52%), Space: 34.7 MB (58.44%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-        vector<bool> ans;
-        for (int i = 0 ; i < l.size() ; ++i) {
-            vector<int> currentSubArray(nums.begin() + l[i], nums.begin() + r[i] + 1);
-            sort(currentSubArray.begin(), currentSubArray.end());
-            int currentDifference = currentSubArray[1] - currentSubArray[0];
-            bool isArithmetic = true;
-            for (int j = 1 ; j < currentSubArray.size() - 1 ; ++j) {
-                if (currentDifference != currentSubArray[j + 1] - currentSubArray[j]) {
-                    isArithmetic = false;
-                    break;
-                }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) 
+    {
+        int m = l.size();
+        vector<bool> res(m,true);
+        for(size_t i =0;i<m;i++)
+        {
+            unordered_set<int> uset;
+            vector<int> subArr(nums.begin()+l[i],nums.begin()+r[i]+1);
+            sort(subArr.begin(),subArr.end());
+            for(int k =1;k<subArr.size();k++)
+            {
+                int diff = subArr[k]-subArr[k-1];
+                if(uset.empty() || uset.find(diff)==uset.end()) uset.insert(diff);
+                if(uset.size()>1) {res[i]=false; break;}
             }
-            ans.push_back(isArithmetic);
         }
-        return ans;
+        return move(res);
     }
 };
