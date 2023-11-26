7d7191d3aca40c31ac6c359e66ebb1eba14a1fb7
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-11-26 19:47:01 +0100

    Time: 140 ms (90.91%), Space: 70.9 MB (60.80%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -4,31 +4,43 @@ public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int m=matrix.size(), n=matrix[0].size();
         int area=count(matrix[0].begin(), matrix[0].end(), 1);
-        #pragma unroll
-        for(int j=0; j<n; j++){
+        if (m==1) return area;
+        if (n==1){
             #pragma unroll
-            for(int i=1;  i<m; i++){
+            for(int i=1; i<m; i++){
+                if (matrix[i][0]!=0)
+                    matrix[i][0]+=matrix[i-1][0];
+                area=max(area, matrix[i][0]);
+            }
+            return area;
+        }
+        
+        #pragma unroll
+        for(int i=1; i<m; i++){
+            #pragma unroll
+            for(int j=0;  j<n; j++){
                 if (matrix[i][j]!=0)
                     matrix[i][j]+=matrix[i-1][j];
             }
-        }
-        unordered_map<int, int> freq;
-        #pragma unroll
-        for(int i=0;  i<m; i++){
+       
             auto row=matrix[i];
-            freq.clear();
-            int maxH=0;
+            int minH=i+1, maxH=0;
             #pragma unroll
             for(int x: row){
+                minH=min(minH, x);
                 maxH=max(maxH, x);
-                freq[x]++;
+            }
+            vector<int> freq(maxH-minH+1, 0);
+            #pragma unroll
+            for(int x: row){
+                freq[x-minH]++;
             }
             int acc=0;
             #pragma unroll
-            for(int x=maxH; acc<n; x--){
-                if (freq.count(x)>0){
+            for(int x=maxH-minH; acc<n; x--){
+                if (freq[x]>0){
                     acc+=freq[x];
-                    area=max(area, acc*x);
+                    area=max(area, acc*(x+minH));
                 }
             }  
         }
