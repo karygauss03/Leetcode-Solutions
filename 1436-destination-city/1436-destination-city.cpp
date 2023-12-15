c737c463a43d46551c8ba5bd53ee9fe8d4ad27aa
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2023-12-15 16:22:14 +0100

    Time: 13 ms (29.07%), Space: 10.8 MB (85.89%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<string> startingCities;
         for(auto& e: paths)  startingCities.insert(e[0]);                        
         for(auto& e: paths) 
-            if(!startingCities.count(e[1])) return e[1];             
+            if(startingCities.find(e[1]) == startingCities.end()) return e[1];             
         return "";
     }
 };
