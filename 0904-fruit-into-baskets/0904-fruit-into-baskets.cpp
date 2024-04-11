16256d7b3df6e51ee0dd5a11a046cd0702e71996
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-04-11 22:28:04 +0100

    Time: 138 ms (14.44%), Space: 74.7 MB (53.06%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,26 +5,26 @@ public:
         set<int> goodTypes;
         unordered_map<int, int> freq;
         for (int j = 0; j < fruits.size(); ++j) {
-            if (goodTypes.size() < 2) {
-                goodTypes.insert(fruits[j]);
+            if (freq.size() < 2) {
+                // goodTypes.insert(fruits[j]);
                 freq[fruits[j]]++;
             }
             else {
-                if (goodTypes.count(fruits[j]) == 1) {
+                if (freq.find(fruits[j]) != freq.end()) {
                     freq[fruits[j]]++;
                 }
                 else {
                     while(freq[fruits[i]] != 0) {
                         freq[fruits[i]]--;
                         if (freq[fruits[i]] == 0) {
-                            goodTypes.erase(fruits[i]);
+                            freq.erase(fruits[i]);
                             ++i;
                             break;
                         }
                         ++i;
                     }
                     freq[fruits[j]]++;
-                    goodTypes.insert(fruits[j]);
+                    // goodTypes.insert(fruits[j]);
                 }
             }
             ans = max(ans, j + 1 - i);
