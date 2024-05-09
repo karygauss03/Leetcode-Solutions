00c0915e32f12561a14ff02ac2ffc7c52317afd5
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-05-09 14:04:04 +0100

    Time: 91 ms (99.85%), Space: 107.3 MB (29.64%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        sort(begin(happiness), end(happiness), greater<int>());
+        int i = 0;
+        long long res = 0;
+
+        while(k--) {
+            happiness[i] = max(happiness[i] - i, 0);
+            res += happiness[i++];
+        }
+
+        return res;
+    }
+};
