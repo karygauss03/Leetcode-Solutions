356f59051bba1a811c98f64a5e544b3c5ad0236d
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-05-24 23:00:25 +0100

    Time: 8 ms (63.88%), Space: 11.5 MB (94.37%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int a[26], b[26], ans = 0;
-    void solve(int idx, vector<string>& words, vector<int>& score) {
-        if (idx == words.size()) {
+    int freq[26], secondFreq[26], ans = 0;
+    void solve(int i, vector<string>& words, vector<int>& score){
+        if (i == words.size()) {
             int sc = 0;
-            for (int i = 0; i < 26; i++) {
-                if (b[i] > a[i])return;
-                sc += score[i] * b[i];
+            for (int i = 0 ; i < 26; ++i) {
+                if (secondFreq[i] > freq[i]) return;
+                sc += score[i] * secondFreq[i];
             }
             ans = max(ans, sc);
             return;
         }
-        solve(idx + 1, words, score);
-        for (auto i : words[idx]) b[i - 'a']++;
-        solve(idx + 1, words, score);
-        for (auto i : words[idx]) b[i - 'a']--;
+        solve(i + 1, words, score);
+        for (auto &c : words[i]) {
+            secondFreq[c - 'a']++;
+        }
+        solve(i + 1, words, score);
+        for (auto &c : words[i]) {
+            secondFreq[c - 'a']--;
+        }
     }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        for (auto i : letters) a[i - 'a']++;
+        for (auto &c : letters) {
+            freq[c - 'a']++;
+        }
         solve(0, words, score);
         return ans;
     }
