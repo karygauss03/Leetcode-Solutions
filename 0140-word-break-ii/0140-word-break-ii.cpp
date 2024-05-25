5c7a8c3eb1edbc5d36c770a5ef221a08ad5fa742
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-05-25 21:30:56 +0100

    Time: 4 ms (32.08%), Space: 8.8 MB (77.44%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> ans;
+    unordered_map<string, int> dict;
+    
+    void solve(string& s, int start, string cur = "") {
+        if (start == s.size()) {
+            cur.pop_back();
+            ans.push_back(cur);
+            return;
+        }
+        
+        for (int end = start; end < s.size(); ++end) {
+            string word = s.substr(start, end - start + 1);
+            if (dict.find(word) != dict.end()) {
+                solve(s, end + 1, cur + word + " ");
+            }
+        }
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        for (auto &word : wordDict) {
+            dict[word] = 1;
+        }
+        solve(s, 0);
+        return ans;
+    }
+};
