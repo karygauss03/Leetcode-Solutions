907b815ac01444474dd18326e30769299871d014
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-06-17 23:14:36 +0100

    Time: 0 ms (100.00%), Space: 7.1 MB (18.55%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (int i = 2 ; i * i <= c ; i++){
+            int cnt = 0;
+            if (c % i == 0){
+                while(c % i == 0){
+                    cnt ++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && cnt % 2 != 0){
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+};
