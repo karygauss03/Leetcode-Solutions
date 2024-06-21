fcf2da4a5fb6c32e0cd6fa6402e32c0ae9e54505
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-06-21 20:56:13 +0100

    Time: 21 ms (77.01%), Space: 34.2 MB (82.34%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int cur_max = 0;
+        int cur_max = 0, n = customers.size();
+        int prefix[n + 1];
+        prefix[0] = 0;
         for (int i = 0; i < grumpy.size(); ++i) {
             if (!grumpy[i]) {
                 cur_max += customers[i];
             }
+            prefix[i + 1] = (grumpy[i] == 1) ? prefix[i] + customers[i] : prefix[i];
         }
         int ans = cur_max;
         for (int i = 0; i <= grumpy.size() - minutes; ++i) {
-            int tmp = cur_max;
-            for (int j = i; j < i + minutes; ++j) {
-                // cout << j << " ";
-                if (grumpy[j]) {
-                    tmp += customers[j];
-                }
-            }
-            // cout << endl;
-            ans = max(ans, tmp);
+            ans = max(ans, cur_max + (prefix[i + minutes] - prefix[i]));
         }
         return ans;
     }
