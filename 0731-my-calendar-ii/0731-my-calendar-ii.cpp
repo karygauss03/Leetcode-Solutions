79f60dbf24a773fd146e1e06c93d5c3efe432ea0
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-09-27 15:45:28 +0100

    Time: 129 ms (78.94%), Space: 44 MB (5.84%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,38 +1,29 @@
 class MyCalendarTwo {
 private:
-    vector<pair<int, int>> bookings;
-    vector<pair<int, int>> overlapIntervals;
-    bool intersect(pair<int, int> interval, int start, int end) {
-        int st = max(interval.first, start);
-        int nd = min(interval.second, end);
-        if (st < nd) {
-            return true;
-        }
-        return false;
-    }
-    
-    pair<int, int> extractOverlap(pair<int, int> booking, int start, int end) {
-        return make_pair(max(booking.first, start), min(booking.second, end));
-    }
-
+    multiset<pair<int, int>> bookings;
 public:
     MyCalendarTwo() {
         bookings.clear();
-        overlapIntervals.clear();
     }
     
     bool book(int start, int end) {
-        for (auto &interval : overlapIntervals) {
-            if (intersect(interval, start, end)) {
+        int cnt = 0;
+        bookings.insert({start, 0});
+        bookings.insert({end - 1, 1});
+        for (auto& booking: bookings) {
+            if (booking.second == 0) {
+                ++cnt;
+            } 
+            if (booking.second == 1) {
+                --cnt;
+            }
+            if (cnt == 3) {
+                bookings.erase(bookings.find({start, 0}));
+                bookings.erase(bookings.find({end - 1, 1}));
                 return false;
             }
         }
-        for (auto &book : bookings) {
-            if (intersect(book, start, end)) {
-                overlapIntervals.push_back(extractOverlap(book, start, end));
-            }
-        }
-        bookings.push_back(make_pair(start, end));
+        
         return true;
     }
 };
