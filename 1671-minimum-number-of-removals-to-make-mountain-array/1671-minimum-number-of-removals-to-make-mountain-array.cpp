fe8e9082798527ecff902c7d020efc6b3785d829
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-10-30 21:33:45 +0100

    Time: 3 ms (92.00%), Space: 15.2 MB (31.93%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,33 +1,22 @@
 class Solution {
 private:
-    int solve(int i, int n, vector<int>& dp, vector<int>& nums, bool reversed = false) {
-        if (i == 0 && !reversed) {
-            return dp[i] = 1;
-        }
-        if (reversed && i == n - 1) {
-            return dp[i] = 1;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        int cur = 1;
-        if (!reversed) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] < nums[i]) {
-                    cur = max(cur, 1 + solve(j, n, dp, nums));
-                }
+    vector<int> LIS(vector<int>& nums) {
+        vector<int> ans(nums.size(), 1);
+        vector<int> bucket = {nums[0]};
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > bucket.back()) {
+                bucket.push_back(nums[i]);
             }
-            return dp[i] = cur;
+            else {
+                int low = lower_bound(bucket.begin(), bucket.end(), nums[i]) - bucket.begin();
+                bucket[low] = nums[i];
+            }
+            ans[i] = bucket.size();
         }
         
-        for (int j = n - 1; j > i; --j) {
-            if (nums[j] < nums[i]) {
-                cur = max(cur, 1 + solve(j, n, dp, nums));
-            }
-        }
-        return dp[i] = cur;
+        return ans;
     }
-    
 
 public:
     int minimumMountainRemovals(vector<int>& nums) {
@@ -35,25 +24,18 @@ public:
         if (n == 3) {
             return 0;
         } 
-        vector<int> dp(n, -1);
-        vector<int> dp2(n, -1);
         
-        // dp[i] = LIS (0, i)
-        for (int i = 0; i < n; ++i) {
-            solve(i, n, dp, nums);
-        }
+        vector<int> lis = LIS(nums);
+        reverse(nums.begin(), nums.end());
+        vector<int> revLis = LIS(nums);
+        reverse(revLis.begin(), revLis.end());
         
-        // dp2[i] = LIS(n - 1, i) reversed
-        for (int i = n - 1; i >= 0; --i) {
-            solve(i, n, dp2, nums, true);
-        }
         int ans = n;
-        for (int i = 1; i < n - 1; ++i) {
-            if (dp[i] > 1 && dp2[i] > 1) {
-                ans = min(ans, n - (dp[i] + dp2[i] - 1));
+        for (int i = 0; i < n; ++i) {
+            if (lis[i] > 1 && revLis[i] > 1) {
+                ans = min(ans, n - lis[i] - revLis[i] + 1);
             }
         }
-
         return ans;
 
     }
