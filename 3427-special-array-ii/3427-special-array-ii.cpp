fef4974e39fe91c7e3c7bc47a316be9619b17de0
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-12-09 22:37:00 +0100

    Time: 1 ms (94.44%), Space: 126.6 MB (69.4%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -3,27 +3,22 @@ public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         vector<bool> ans;
         int n = nums.size();
-        vector<int> far(n, 0);
-        int l = 0;
+        vector<int> prefix(n, 0);
         for (int i = 1; i < n; ++i) {
-            if (nums[i] % 2 == nums[i - 1] % 2) {
-                for (int j = l; j <= i - 1; ++j) {
-                    far[j] = i - 1;
-                }
-                l = i;
+            if (nums[i] % 2 != nums[i - 1] % 2) {
+                prefix[i] = prefix[i - 1];
+            } 
+            else {
+                prefix[i] = prefix[i - 1] + 1;
             }
+            // cout << "i: " << i << " " << prefix[i] << endl;
         }
-        for (int j = l; j < n; ++j) {
-            far[j] = n - 1;
-        }
+
         for (auto& q : queries) {
-            int st = q[0], nd = q[1];
-            if (nd <= far[st]) {
+            if (prefix[q[1]] - prefix[q[0]] == 0) {
                 ans.push_back(true);
             }
-            else {
-                ans.push_back(false);
-            }
+            else ans.push_back(false);
         }
         return ans;
     }
