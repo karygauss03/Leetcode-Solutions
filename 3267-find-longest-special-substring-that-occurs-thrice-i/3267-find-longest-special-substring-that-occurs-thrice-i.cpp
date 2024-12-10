0ab413536aa1d088ff62d33c4022faed0305ebbf
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2024-12-10 22:26:27 +0100

    Time: 4 ms (90.8%), Space: 10.8 MB (80.62%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int maximumLength(string s) {
         int n = s.size();
-        vector<vector<int>> freq(26, vector<int>(n + 1, 0));
-        int cur = 1, ans = -1;
+        vector<vector<int>> freq(26, vector<int>(3, -1));
+        int cur = 0, ans = -1;
         char prev = s[0];
-        freq[prev - 'a'][cur] = 1;
-        for (int i = 1; i < n; ++i) {
-            if (s[i] == prev) {
+        for (auto &c : s) {
+            if (c == prev) {
                 ++cur;
-                freq[s[i] - 'a'][cur] += 1;
             }
             else {
                 cur = 1;
-                prev = s[i];
-                freq[s[i] - 'a'][1] += 1;
+                prev = c;
+            }
+
+            auto it = min_element(freq[c - 'a'].begin(), freq[c - 'a'].end());
+            if (cur > *it) {
+                // cout << "before: " << *it << endl;
+                *it = cur;
+                // cout << "after: " << *it << endl;
             }
         }
 
-        for (int j = 0 ; j < 26; ++j) {
-            for (int i = n - 1; i >= 1; --i) {
-                freq[j][i] += freq[j][i + 1];
-                if (freq[j][i] >= 3) {
-                    ans = max(ans, i);
-                    break;
-                }
-            }
+        for (int i = 0; i < 26; ++i) {
+            ans = max(ans, *min_element(freq[i].begin(), freq[i].end()));
         }
 
         return ans;
