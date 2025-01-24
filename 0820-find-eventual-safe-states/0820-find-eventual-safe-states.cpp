6cc0d9c2905b9f7aabcaa036d92f6dcbcfdb31eb
Karim Omrane <72917840+karygauss03@users.noreply.github.com>
2025-01-24 10:53:27 +0100

    Time: 8 ms (79.05%), Space: 51.9 MB (88.6%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> col;
+    
+    bool dfs(vector<vector<int>> &adj, int node) {
+        if (col[node] > 0) {
+            return col[node] == 2;
+        }
+        col[node] = 1;
+        for (auto u : adj[node]) {
+            if (col[u] == 1 || !dfs(adj, u)) {
+                return false;
+            }
+        }
+        col[node] = 2;
+        return true;
+    }
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        col.resize(n, 0);
+        for (int i = 0; i < n ; ++i) {
+            if (dfs(graph, i)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
